302172ff47edfe225b8cf95f47d91ab24756b4a4
Daniel Mangowi <mangowidaniel@gmail.com>
2017-07-08 14:34:52 +0200

    adding event command on constructor

--- DJMData/TrainingProductViewModel.cs
+++ DJMData/TrainingProductViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,11 @@ namespace DJMData
         public TrainingProductViewModel()
         {
             Products = new List<TrainingProduct>();
+            EventCommand = "List";
         }
 
         public List<TrainingProduct> Products { get; set; }
+       
         public string EventCommand { get; set; }
 
 
@@ -25,6 +28,8 @@ namespace DJMData
                 case "list":
                     Get();
                     break;
+
+
                 default:
                     break;
             }
