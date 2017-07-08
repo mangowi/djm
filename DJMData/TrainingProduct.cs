8af321b22f2ecc86f8fadd60f2f8c867d0c5d449
Daniel Mangowi <mangowidaniel@gmail.com>
2017-07-08 14:03:53 +0200

    training product class and product manage, and add data

--- DJMData/TrainingProduct.cs
+++ DJMData/TrainingProduct.cs
@@ -8,6 +8,10 @@ namespace DJMData
 {
     public class TrainingProduct
     {
-
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public DateTime IntroductionDate { get; set; }
+        public string Url { get; set; }
+        public decimal Price { get; set; }
     }
 }
---
+++ DJMData/TrainingProductManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DJMData
+{
+   public  class TrainingProductManager
+    {
+
+        private List<TrainingProduct> CreateMockData()
+        {
+            
+            List<TrainingProduct> ret = new List<TrainingProduct>();
+
+
+            ret.Add(new TrainingProduct()
+            {
+                ProductId = 1,
+                ProductName = "ASP .NET MVC",
+                IntroductionDate = Convert.ToDateTime("6/11/2017"),
+                Url = "https//:mangowi.com",
+                Price = Convert.ToDecimal(20.00)
+            });
+
+            return ret;
+        }
+    }
+}
